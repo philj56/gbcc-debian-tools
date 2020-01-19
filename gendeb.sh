10421f6e1f04ee50f0e61c6e1a1b80b16c89627f
Phil Jones <philj56@gmail.com>
2020-01-19 18:39:00 +0000

    Initial commit of debian packaging tools for gbcc.

---
+++ gendeb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+rev=$(git -C gbcc rev-list --count HEAD)
+
+version=0.1.$rev
+
+mkdir -p debbuild && cd debbuild
+
+cp -r ../gbcc gbcc-$version
+
+files=(
+	gbcc-$version/completions/
+	gbcc-$version/doc/
+	gbcc-$version/shaders/
+	gbcc-$version/src/
+	gbcc-$version/meson.build
+	gbcc-$version/meson_options.txt
+	gbcc-$version/tileset.png
+	gbcc-$version/print.wav
+	gbcc-$version/LICENSE
+)
+
+tar cJf gbcc_$version.orig.tar.xz "${files[@]}"
+rm -rf gbcc-$version
+
+tar xf gbcc_$version.orig.tar.xz
+cd gbcc-$version
+
+cp -r ../../debian .
+cp LICENSE debian/copyright
+
+grep -rlI GBCC_GIT_REVISION debian | xargs sed -i "s/GBCC_GIT_REVISION/$rev/g"
+
+debuild -us -uc
